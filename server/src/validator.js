01ab80616130c720640a2e4229ce1884b1c7c322
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-02-08 00:59:15 -0800

    added validator message tracking obj

--- server/src/validator.js
+++ server/src/validator.js
@@ -1,28 +1,42 @@
+const validatorData = {};
+
 const check = (field) => {
     return {
         isPassword: () => {
-            if (field.length < 6 || field.length > 30) return false;
+            if (field.length < 6 || field.length > 30) {
+                validatorData.msg = "Password must be between 6 and 30 characters.";
+                return false;
+            }
             let re = /^[a-zA-Z0-9 !"#\$%&'\(\)\*\+,-\./:;<=>\?@\[\\\]\^_`\{\|\}~]+$/;
-            return re.test(field);
+            let res = re.test(field);
+            if (!res) validatorData.msg = "Password contains invalid characters.";
+            return res;
         },
         isEmail: () => {
             let re = /\S+@\S+\.\S+/;
-            return re.test(field);
+            let res = re.test(field);
+            if (!res) validatorData.msg = "Email is an incorrect format.";
+            return res;
         },
         isUsername: () => {
-            if (field.length < 4 || field.length > 22) return false;
+            if (field.length < 4 || field.length > 22) {
+                validatorData.msg = "Username must be between 4 and 22 characters.";
+                return false;
+            }
             let re = /^[a-zA-Z0-9-_.]+$/;
-            return re.test(field);
+            let res = re.test(field);
+            if (!res) validatorData.msg = "Username contains invalid characters.";
+            return res;
         },
     }
 }
 
 // tests
-console.log(check('!"#$%&\'()*+,-./:;<=>?@').isPassword());
-console.log(check('[\]^_` {|}~').isPassword());
-console.log(check('[\]^_` {|}~').isUsername());
-console.log(check('spaceRanger-69_hi').isUsername());
+// console.log(check('!"#$%&\'()*+,-./:;<=>?@').isPassword());
+// console.log(check('[\]^_` {|}~').isPassword());
+// console.log(check('[\]^_` {|}~').isUsername());
+// console.log(check('spaceRanger-69_hi').isUsername());
 
 module.exports = {
-    check
+    check, validatorData
 };

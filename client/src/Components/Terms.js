a2e45e733d73ff87f381c838fe52dd63df3984cb
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-02-08 00:58:41 -0800

    updated terms of service

--- client/src/Components/Terms.js
+++ client/src/Components/Terms.js
@@ -11,14 +11,15 @@ class Terms extends React.Component {
             <div className="content">
                 <div>
                     <h1>Terms of Service</h1>
-                    <h4>
-                        gram's ban list:
-                    </h4>
                     <ul>
-                        <li>anyone with an onlyfans</li>
-                        <li>anyone with a pocketstars</li>
-                        <li>anyone who has done hot tub meta</li>
-                        <li>hailee steinfield</li>
+                        <li>You don't talk about gram.</li>
+                        <li>You don't talk about gram.</li>
+                        <li>When someone says stop, or taps out, or goes limp, the fight is over.</li>
+                        <li>Only two guys to a fight</li>
+                        <li>One fight at a time.</li>
+                        <li>No shirts, no shoes.</li>
+                        <li>Fights will go on as long as they have to.</li>
+                        <li>If this is your fight night at Fight Club, you <em>have</em> to fight.</li>
                     </ul>
                 </div>
 
